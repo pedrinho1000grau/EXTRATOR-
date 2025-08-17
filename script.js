f1480e0de82688fbf3af90ba51cda7af4c995337
pedrinho1000grau <pedro.rodrigues.oliveira20@escola.pr.gov.br>
2025-08-17 15:05:34 -0300

    ok5

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ function mostraPalavrasChave (){
     const campoResultado = document.querySelector('#resultado-palavrachave');
     const palavras = texto.split(" ");
 
-    campoResultado.textContent = texto;
+    campoResultado.textContent = palavras;
 }
