e8efe066ac966dfb3904fe65cd1e1ffc851c39ed
pedrinho1000grau <pedro.rodrigues.oliveira20@escola.pr.gov.br>
2025-08-17 13:58:29 -0300

    ok3

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave (){
-    alert('atenção!');
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+
+    campoResultado.textContent = texto;
 }
